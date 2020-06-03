f3611adfaca5cb2821027767bc37739c79b0b3a1
Bryan Reese <bryan.mreese@gmail.com>
2020-06-03 17:17:43 -0400

    First changes to personalize

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Nenive.net', // Navigation and Site Title
+  siteTitleAlt: 'Nenive.net - Art Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Nenive', // short_name for manifest
+  siteHeadline: 'The art of Sara Rodrigues', // Headline for schema.org JSONLD
+  siteUrl: 'https://nenive.net', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Digital art as an example of what I like to create',
+  author: 'Sara Rodrigues', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@jodie', // Twitter Username
@@ -18,6 +18,6 @@ module.exports = {
   googleAnalyticsID: 'UA-XXXXXX-X',
 
   // Manifest and Progress color
-  themeColor: '#db7436',
-  backgroundColor: '#3b3c4f',
+  themeColor: 'white',
+  backgroundColor: 'blue',
 }
